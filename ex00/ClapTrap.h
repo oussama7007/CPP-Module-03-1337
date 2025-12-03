19841c25646687f798fc9e7d569f617fed388ecc
oussama7007 <oussamatachakourt@gmail.com>
2025-12-03 11:56:05 +0100

    Auto update: 2025-12-03 11:56:05

--- ex00/ClapTrap.h
+++ ex00/ClapTrap.h
@@ -6,7 +6,7 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 17:26:05 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/12/03 10:55:45 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/12/03 11:00:15 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,10 @@ class ClapTrap
     public:
         ClapTrap();
         ClapTrap(const ClapTrap& obj);        
+        ClapTrap(std::string Name);
+        ClapTrap& operator=(const ClapTrap& obj);
+        ~ClapTrap();
 
-
-
-
-        
         void    attack(const std::string& target);
         void    takeDamage(unsigned int amount);
         void    beRepaired(unsigned int amount);
