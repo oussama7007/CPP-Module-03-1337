de1797bc116835ffd06b96d9cf116bfb5b365b58
oussama7007 <oussamatachakourt@gmail.com>
2025-12-05 11:10:54 +0100

    Auto update: 2025-12-05 11:10:54

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 17:26:00 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/12/05 10:10:35 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/12/05 10:56:22 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,81 @@ ClapTrap::ClapTrap(std::string Name) : Name(Name), Hit(10), Energy(10), Attack(0
     std::cout << "ClapTrap " << Name << " is born!" << std::endl;
 }
 
+ClapTrap::ClapTrap(const ClapTrap& obj)
+{
+    std::cout << "ClapTrap Copy Constructor called" << std::endl;
+    *this = obj;
+}
+
+
+ClapTrap&  ClapTrap::operator=(const ClapTrap& obj)
+{
+    std::cout << "ClapTrap Copy Assignment Operator called" << std::endl;
+    if(this != &obj)
+    {
+        this->Name = obj.Name;
+        this->Hit = obj.Hit;
+        this->Energy = obj.Energy;
+        this->Attack = obj.Attack;
+    }
+    return *this;
+}
+
+
+void    ClapTrap::attack(const std::string& target)
+{
+    if(Energy <= 0)
+    {
+        std::cout << "ClapTrap " << Name << " has no energy left to attack!" << std::endl;
+        return;        
+    }
+    if(Hit <= 0)
+    {
+        std::cout << "ClapTrap " << Name << " is dead and cannot attack!" << std::endl;
+        return;
+    }
+    Energy--;
+    std::cout << "ClapTrap " << Name << " attacks " << target 
+              << ", causing " << Attack << " points of damage!" << std::endl;
+}
+
+void        ClapTrap::takeDamage(unsigned int amount)
+{
+    if(Hit <= 0)
+    {
+        std::cout << "ClapTrap " << Name << " is already dead!" << std::endl;
+        return;
+    }
+    if((int)amount >= Hit)
+        Hit = 0;
+    else 
+        Hit -= amount;
+    std::cout << "ClapTrap " << Name << " takes " << amount 
+              << " points of damage! (Current HP: " << Hit << ")" << std::endl;
+}
+
+void    ClapTrap::beRepaired(unsigned int amount)
+{
+    if (Energy <= 0)
+    {
+        std::cout << "ClapTrap " << Name << " has no energy left to repair!" << std::endl;
+        return;
+    }
+    if (Hit <= 0)
+    {
+        std::cout << "ClapTrap " << Name << " is dead and cannot be repaired!" << std::endl;
+        return;
+    }
+    Energy--;
+    Hit += amount;
+    std::cout << "ClapTrap " << Name << " repairs itself, recovering " 
+              << amount << " hit points! (Current HP: " << Hit << ")" << std::endl;
+}
+
+
 
 ClapTrap::~ClapTrap()
 {
     std::cout << "ClapTrap " << Name << " destroyed." << std::endl;
 }
+
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,9 +6,40 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 17:26:09 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/12/05 10:07:55 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/12/05 11:10:47 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.h"
 
+int main()
+{
+  
+    std::cout << "--- CREATION ---" << std::endl;
+    ClapTrap robot("");
+    
+    
+    std::cout << "\n--- ATTACK TEST ---" << std::endl;
+    robot.attack("Target Dummy");
+    
+  
+    std::cout << "\n--- DAMAGE TEST ---" << std::endl;
+    robot.takeDamage(5); 
+
+    std::cout << "\n--- REPAIR TEST ---" << std::endl;
+    robot.beRepaired(3); 
+
+  
+    std::cout << "\n--- DEATH TEST ---" << std::endl;
+    robot.takeDamage(100); 
+    robot.attack("Target Dummy");
+    robot.beRepaired(10); 
+
+    std::cout << "\n--- ENERGY DEPLETION TEST (New Robot) ---" << std::endl;
+    ClapTrap robot2("*el3erbi*");
+    for(int i = 0; i < 12; i++) 
+        robot2.attack("Spam Target");
+
+    std::cout << "\n--- DESTRUCTION ---" << std::endl;
+    return 0;
+}
