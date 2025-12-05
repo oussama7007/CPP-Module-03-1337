151336e5e37f621836b335b1c6cc20bc464f167d
oussama7007 <oussamatachakourt@gmail.com>
2025-12-05 17:49:51 +0100

    Auto update: 2025-12-05 17:49:51

--- ex01/ScavTrap.h
+++ ex01/ScavTrap.h
@@ -6,7 +6,27 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 11:58:38 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/12/05 11:58:39 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/12/05 17:43:53 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+#include "ClapTrap.h"
+
+
+class ScavTrap : public ClapTrap
+{
+    public:
+        ScavTrap();
+        ScavTrap(const ScavTrap& obj);
+        ScavTrap(std::string Name);
+        ScavTrap& operator=(const ScavTrap& obj);
+        ~ScavTrap();
+        
+        void    guardGate();
+};
+
+#endif
