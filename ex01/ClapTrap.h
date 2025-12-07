e3d4aecd8d4e239021707ed67d95b4ee585a7314
oussama7007 <oussamatachakourt@gmail.com>
2025-12-07 14:15:19 +0100

    ba9i makemetl f mac

--- ex01/ClapTrap.h
+++ ex01/ClapTrap.h
@@ -6,7 +6,7 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 11:58:26 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/12/06 14:59:40 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/12/07 14:07:35 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ class ClapTrap
         ClapTrap(const ClapTrap& obj);        
         ClapTrap(std::string   Name);
         ClapTrap& operator=(const ClapTrap& obj);
-        virtual ~ClapTrap();
+        virtual ~ClapTrap(); 
 
         void    attack(const std::string& target);
         void    takeDamage(unsigned int amount);
